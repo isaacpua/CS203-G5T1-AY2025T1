7f463c071185b83ce25b79ce659b8ec21e89dfbd
Julian <juliandominic@yahoo.com.sg>
2025-08-29 01:54:50 +0800

    feat: added the update api to update the tariff rows based on the id

--- tariff-backend/src/main/java/com/tariff/tariff_backend/controller/DashboardController.java
+++ tariff-backend/src/main/java/com/tariff/tariff_backend/controller/DashboardController.java
@@ -1,16 +1,16 @@
 package com.tariff.tariff_backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tariff.tariff_backend.model.Tariff;
-import com.tariff.tariff_backend.model.dashboard.CreateResponse;
+import com.tariff.tariff_backend.model.dashboard.DashboardResponse;
+import com.tariff.tariff_backend.model.dashboard.TariffPatchDTO;
 import com.tariff.tariff_backend.service.DashboardService;
 
 @RestController
@@ -25,7 +25,12 @@ public class DashboardController {
     }
 
     @PostMapping("/tariffs")
-    public CreateResponse createTariff(@RequestBody Tariff request) {
+    public DashboardResponse createTariff(@RequestBody Tariff request) {
         return dashboardService.createTariff(request);
+    }
+
+    @PatchMapping("/tariffs/{id}")
+    public DashboardResponse updateTariff(@PathVariable Integer id, @RequestBody TariffPatchDTO patchDTO) {
+        return dashboardService.updateTariff(id, patchDTO);
     }
 }
--- tariff-backend/src/main/java/com/tariff/tariff_backend/service/DashboardService.java
+++ tariff-backend/src/main/java/com/tariff/tariff_backend/service/DashboardService.java
@@ -1,10 +1,13 @@
 package com.tariff.tariff_backend.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tariff.tariff_backend.model.Tariff;
 import com.tariff.tariff_backend.model.dashboard.DashboardResponse;
+import com.tariff.tariff_backend.model.dashboard.TariffPatchDTO;
 import com.tariff.tariff_backend.repository.TariffRepo;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +24,7 @@ public class DashboardService {
         Integer hts8 = newTariff.getHts8();
         try {
             if (!tariffRepo.findByHts8(hts8).isEmpty()) {
-                throw new Exception("Unable to create new tariff because the hts8 code provided already exists in the database.");
+                throw new Exception("Unable to create new tariff because the HTS8 code " + hts8 + " already exists.");
             }
 
             Tariff savedTariff = tariffRepo.save(newTariff);
@@ -29,6 +32,41 @@ public class DashboardService {
             if (savedTariff == null || tariffRepo.findByHts8(hts8).isEmpty()) {
                 throw new Exception("Unable to create the new tariff.");
             }
+        } catch (Exception e) {
+            response.setSuccess(false);
+            response.setMessage(e.getMessage());
+        }
+        return response;
+    }
+
+    public DashboardResponse updateTariff(Integer id, TariffPatchDTO patchDTO) {
+        DashboardResponse response = new DashboardResponse(true, "Sucessfully updated the tariff.");
+        Integer hts8 = patchDTO.getHts8();
+        String briefDescription = patchDTO.getBriefDescription();
+        String mfnTextRate = patchDTO.getMfnTextRate();
+        try {
+            Optional<Tariff> optionalTariff = tariffRepo.findById(id);
+            if (optionalTariff.isEmpty()) {
+                throw new Exception("Tariff with ID " + id + " not found");
+            }
+            Tariff existingTariff = optionalTariff.get();
+
+            if (hts8 != null) {
+                if (!tariffRepo.findByHts8(hts8).isEmpty()) {
+                    throw new Exception("Unable to create new tariff because the HTS8 code " + hts8 + " already exists.");
+                }
+                existingTariff.setHts8(hts8);
+            }
+
+            if (briefDescription != null) {
+                existingTariff.setBrief_description(briefDescription);
+            }
+
+            if (mfnTextRate != null) {
+                existingTariff.setMfn_text_rate(mfnTextRate);
+            }
+
+            tariffRepo.save(existingTariff);
         } catch (Exception e) {
             response.setSuccess(false);
             response.setMessage("Internal server error. " + e.getMessage());
