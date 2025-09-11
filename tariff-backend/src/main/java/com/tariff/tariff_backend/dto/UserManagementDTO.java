689889c71a2387487c8090f21901cf74e16dcb36
Julian <juliandominic@yahoo.com.sg>
2025-09-11 15:02:59 +0800

    refactor: updated UserManagementDTO to make some fields optional

--- tariff-backend/src/main/java/com/tariff/tariff_backend/dto/UserManagementDTO.java
+++ tariff-backend/src/main/java/com/tariff/tariff_backend/dto/UserManagementDTO.java
@@ -1,14 +1,25 @@
 package com.tariff.tariff_backend.dto;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserManagementDTO {
     private UUID id;
     private String username;
     private String roles;
+
+    // Constructor without ID for create operations
+    public UserManagementDTO(String username, String roles) {
+        this.username = username;
+        this.roles = roles;
+        this.id = null;
+    }
 }
