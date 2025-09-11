798a647ab99772290d2956d9bfaca7f80f911169
Julian <juliandominic@yahoo.com.sg>
2025-09-11 13:26:04 +0800

    fix: imported HistoricalTariffExplorer

--- tariff-frontend/src/App.jsx
+++ tariff-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import { useState } from 'react';
+import HistoricalTariffExplorer from './components/HistoricalTariffExplorer';
 
 function App() {
   return (
