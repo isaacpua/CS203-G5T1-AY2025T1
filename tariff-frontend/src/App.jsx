b1cec76b37db69184b0983360a6721f4423179f0
Julian <juliandominic@yahoo.com.sg>
2025-09-11 13:17:28 +0800

    fix: ThemeProvider import

--- tariff-frontend/src/App.jsx
+++ tariff-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import { useState } from 'react';
+import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
