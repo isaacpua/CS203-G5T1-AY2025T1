b19a7ec0542cb0904044cc087d3b854e4edad520
Ernest Ng <168986276+ernestng3@users.noreply.github.com>
2025-09-23 15:33:11 +0800

    fix: Reorder import statements for consistency
    Moved import statements in SecurityConfiguration and TariffController to group project-specific imports after standard library imports, improving code readability and consistency.

--- tariff-backend/src/main/java/com/tariff/tariff_backend/controller/TariffController.java
+++ tariff-backend/src/main/java/com/tariff/tariff_backend/controller/TariffController.java
@@ -1,10 +1,11 @@
 package com.tariff.tariff_backend.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
