204d529dcf77d86816a523138c7a90823e3aa694
isaacpua <pua.isaac@gmail.com>
2025-10-24 14:58:00 +0800

    fix: add sorting to getAllTariffs method in DashboardService

--- tariff-backend/src/main/java/com/tariff/tariff_backend/service/DashboardService.java
+++ tariff-backend/src/main/java/com/tariff/tariff_backend/service/DashboardService.java
@@ -6,13 +6,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.tariff.tariff_backend.model.tariffs_new.Country;
-import com.tariff.tariff_backend.model.tariffs_new.Tariff;
 import com.tariff.tariff_backend.model.dashboard.DashboardMetrics;
 import com.tariff.tariff_backend.model.dashboard.DashboardResponse;
 import com.tariff.tariff_backend.model.dashboard.TariffPatchDTO;
+import com.tariff.tariff_backend.model.tariffs_new.Country;
+import com.tariff.tariff_backend.model.tariffs_new.Tariff;
 import com.tariff.tariff_backend.repository.CountryRepo;
 import com.tariff.tariff_backend.repository.TariffRepo;
 
@@ -163,7 +164,8 @@ public class DashboardService {
     }
 
     public List<TariffPatchDTO> getAllTariffs() {
-        return tariffRepo.findAll().stream()
+        Sort sort = Sort.by(Sort.Direction.ASC, "tariffId");
+        return tariffRepo.findAll(sort).stream()
                 .map(tariff -> new TariffPatchDTO(
                         tariff.getTariffId(),
                         tariff.getDescriptionwcountry(),
