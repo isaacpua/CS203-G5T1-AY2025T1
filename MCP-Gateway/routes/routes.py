08957c47f55de7142e921de85d72231b73c7e656
Given Liu <givenliu03@gmail.com>
2025-10-28 01:29:12 +0800

    feat: added /analyze endpoint and improved response parsing for newsletter endpoint

--- MCP-Gateway/routes/routes.py
+++ MCP-Gateway/routes/routes.py
@@ -79,9 +79,38 @@ async def get_newsletter():
     try:
         async with client:
             logging.info(f"Calling newsletter tool on {MCP_SERVER_URL} ...")
-            response = await client.call_tool("newsletter_scrape")
+            mcp_response = await client.call_tool("newsletter_scrape")
             logging.info(f"Successfully called newsletter tool!")
+
+            response = json.loads(mcp_response.content[0].text)
             print(response)
+
+            # with open("response.md", "w", encoding="utf-8") as file:
+            #     print("Writing md to file...")
+            #     file.write(response["markdown"])
+            return response
+        
+    except Exception as e:
+        logging.error(f"Error details: {e}")
+        return {"error": e}
+    
+
+
+# !! INCOMPLETE !!
+@router.get("/analyze")
+async def analyze(url: str):
+    try:
+        async with client:
+            logging.info(f"Calling scrape tool on {MCP_SERVER_URL} ...")
+            mcp_response = await client.call_tool("scrape_single_url", {"url": url})
+            logging.info(f"Successfully called scrape tool!")
+
+            response = json.loads(mcp_response.content[0].text)
+            print(response)
+
+            # with open("response.md", "w", encoding="utf-8") as file:
+            #     print("Writing md to file...")
+            #     file.write(response["markdown"])
             return response
         
     except Exception as e:
