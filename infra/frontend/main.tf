cdcc54918e9a9d883695a566db4f552a197f159b
Julian <juliandominic@yahoo.com.sg>
2025-11-07 18:24:33 +0800

    chore: increased cloudfront timeout to 60s

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -132,6 +132,8 @@ resource "aws_cloudfront_distribution" "spa" {
       https_port             = 443
       origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1.2"]
+      origin_read_timeout    = 60
+      origin_keepalive_timeout = 5
     }
   }
 
